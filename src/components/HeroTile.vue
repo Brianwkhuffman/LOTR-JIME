d3863b0b25af0d84a21f0dd635c9a06a0c9ce1c0
Brian Huffman <brianwkhuffman@gmail.com>
2025-08-03 19:34:13 -1000

    Updates HeroTile component
    Moves logic and elements from HeroList to Tile
    Adds some quasar components and styling

--- src/components/HeroTile.vue
+++ src/components/HeroTile.vue
@@ -1,3 +1,64 @@
+<script setup>
+import {useRouter} from 'vue-router';
+
+const router = useRouter();
+
+/**
+ * @typedef {Object} hero
+ * @property {string} name
+ */
+const { hero } = defineProps({
+  hero: {
+    type: Object,
+    required: true
+  }
+})
+
+const goToHeroDetails = (name) => {
+  router.push('/heroes/' + name);
+}
+
+const selectHero = (name) => {
+  // TODO: handle selected hero
+  alert(name + ' selected!');
+}
+
+</script>
+
 <template>
-  <h1>Hero Tile</h1>
+  <q-card class="hero-tile q-mb-md">
+    <q-card-section>
+      <div class="text-h6">{{hero.name}}</div>
+      <div class="text-subtitle2">Race: {{hero.race}}</div>
+      <div
+        class="row"
+        v-for="(stat, key) in hero.stats"
+        :key="key"
+      >
+        <div class="col text-capitalize">{{key}}: {{stat}}</div>
+      </div>
+    </q-card-section>
+    <q-separator />
+
+    <q-card-section>
+      {{hero.ability}}
+    </q-card-section>
+
+    <q-card-actions>
+      <q-btn @click="goToHeroDetails(hero.name.toLowerCase())">
+        Details
+      </q-btn>
+      <q-btn @click="selectHero(hero.name)">
+        Select Hero
+      </q-btn>
+    </q-card-actions>
+
+  </q-card>
 </template>
+
+<style lang="scss" scoped>
+.hero-tile {
+  color: saddlebrown;
+  width: 75%
+}
+</style>
--- src/pages/HeroList.vue
+++ src/pages/HeroList.vue
@@ -1,46 +1,24 @@
 <script setup>
-import { storeToRefs } from 'pinia';
-import { useRouter } from 'vue-router';
-import { useHeroDetailsStore } from '/src/stores/heroDetailsStore.js';
+import {storeToRefs} from 'pinia';
+import {useHeroDetailsStore} from '/src/stores/heroDetailsStore.js';
 import SimpleNavButton from '/src/components/SimpleNavButton.vue';
+import HeroTile from '/src/components/HeroTile.vue';
 
-const router = useRouter();
 const heroStore = useHeroDetailsStore();
 const { heroes } = storeToRefs(heroStore);
 
-const goToHeroDetails = (name) => {
-  router.push('/heroes/' + name);
-};
 </script>
 
 <template>
   <div>
-    <div style="border:1px solid black">
-      <h1>Heroes of Middle-earth</h1>
-      <SimpleNavButton target-page="home"/>
+    <div class="text-h4">Heroes of Middle-earth
+    <simple-nav-button target-page="home"/>
     </div>
 
-    <div style="border: 1px solid black; margin-top: 8px;">
-      <div v-for="(hero, key) in heroes" :key="hero.id">
-        <div>
-          <span>🧙</span>
-        </div>
-
-        <div>
-          <div>
-            <h2>{{ hero.name }}</h2>
-            <span>
-              {{ hero.race }}
-            </span>
-          </div>
-
-          <div>
-            <q-btn @click="goToHeroDetails(key)">
-              Details
-            </q-btn>
-          </div>
-        </div>
-      </div>
-    </div>
+    <hero-tile
+      v-for="hero in heroes"
+      :hero="hero"
+      :key="hero.id"
+    />
   </div>
 </template>
