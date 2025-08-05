d8296449dbe7e80e3f7c790e0020b457ca7d6618
Brian Huffman <brianwkhuffman@gmail.com>
2025-08-04 21:30:39 -1000

    Renamed hero detail store (non plural)

--- src/stores/heroDetailStore.js
+++ src/stores/heroDetailStore.js
@@ -1,17 +1,21 @@
-import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import {defineStore} from 'pinia';
+import {ref} from 'vue';
 import heroDetails from '/src/data/heroDetails.json';
 
-export const useHeroDetailsStore = defineStore('heroes', () => {
+export const useHeroDetailStore = defineStore('heroDetail', () => {
   const heroes = ref(heroDetails);
 
   const getHeroList = () => {
     return Object.keys(heroes);
-  };
+  }
 
   const getHeroDetailsByName = (name) => {
     return heroes.value[name];
-  };
+  }
 
-  return { heroes, getHeroList, getHeroDetailsByName };
+  return {
+    heroes,
+    getHeroList,
+    getHeroDetailsByName,
+  }
 });
