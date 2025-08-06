6ffe62fd1a36506c6703f084decaf03a82a08ca4
Brian Huffman <brianwkhuffman@gmail.com>
2025-08-05 23:34:01 -1000

    Setup for equip type list and equip list

--- src/pages/cards/EquipmentTypeList.vue
+++ src/pages/cards/EquipmentTypeList.vue
@@ -1,7 +1,31 @@
 <script setup>
+import {useRouter} from 'vue-router';
+import {useEquipmentStore} from '/src/stores/equipmentStore.js';
 
+const router = useRouter();
+const equipStore = useEquipmentStore();
+const equipTypes = equipStore.getEquipmentTypes;
+
+const goToType = (type) => {
+  router.push('/cards/equipment/' + type);
+};
 </script>
 
 <template>
-  <h1>hi</h1>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <q-card
+        v-for="type in equipTypes"
+        :key="type"
+        @click="goToType(type)"
+        class="cursor-pointer hoverable"
+        flat
+        bordered
+      >
+        <q-card-section class="text-center">
+          <div class="text-h6 text-capitalize">{{ type }}</div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
 </template>
