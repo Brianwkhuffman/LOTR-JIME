e6530378b6beb99c435aad87760143d408dc9ffb
Brian Huffman <brianwkhuffman@gmail.com>
2025-08-05 23:33:44 -1000

    More whitespace fixes

--- src/pages/cards/CardDirectory.vue
+++ src/pages/cards/CardDirectory.vue
@@ -2,17 +2,15 @@
 import {useRouter} from 'vue-router';
 
 const router = useRouter();
-const cardTypes = ['heroes', 'equipment', 'roles', 'titles', 'weakness', 'damage/fear', 'info'];
+const cardTypes = ['heroes', 'equipment', 'roles', 'titles', 'weakness', 'damageFear', 'info'];
 
-function goToType(type) {
+const goToType = (type) => {
   router.push('/cards/' + type);
 };
 </script>
 
 <template>
   <div class="q-pa-md">
-    <div class="text-h5 q-mb-lg rounded-borders">Directory</div>
-
     <div class="q-gutter-sm">
       <q-card
         v-for="type in cardTypes"
