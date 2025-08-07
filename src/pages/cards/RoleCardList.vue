0650d42c40508f0ce7cb3db3bb84d053999ac91f
Brian Huffman <brianwkhuffman@gmail.com>
2025-08-06 22:13:00 -1000

    Implements RoleCardList
    Left todo for fate/success icons

---
+++ src/pages/cards/RoleCardList.vue
@@ -0,0 +1,46 @@
+<script setup>
+import {useRoleCardStore} from '/src/stores/roleCardStore.js';
+import {useRoute} from 'vue-router';
+import BackButton from 'src/components/button/BackButton.vue';
+
+const route = useRoute();
+const roleParam = route.params.role;
+const roleCardStore = useRoleCardStore();
+const roleCards = roleCardStore.getRoleCardListByRole(roleParam);
+
+</script>
+
+<template>
+  <back-button/>
+  <section class="q-pa-lg">
+    <q-card
+      v-for="card in roleCards"
+      :key="card.id"
+      bordered
+      class="q-mb-md"
+    >
+      <q-card-section class="row justify-between">
+        <div>
+          <h5>{{ card.name }}
+
+            <!-- TODO: Change icons... -->
+            <q-icon v-if="card.icon === '1 Success'" color="green" name="star"/>
+            <q-icon v-else-if="card.icon === '1 Fate'" color="red" name="emergency"/>
+            <q-icon v-else color="blue" name="emergency"/>
+
+          </h5>
+          <i class="it">{{card.trait}}</i>
+        </div>
+        <q-img
+          src="/assets/rolePh.png"
+          alt="Role image"
+          style="width: 80px; height: auto"
+          class="rounded-borders"
+        />
+      </q-card-section>
+      <q-card-section>
+        <p>{{ card.description }}</p>
+      </q-card-section>
+    </q-card>
+  </section>
+</template>
