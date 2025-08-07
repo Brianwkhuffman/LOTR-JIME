e46f3ba2725cba139627641a814b4523c4e8aee9
Brian Huffman <brianwkhuffman@gmail.com>
2025-08-06 20:49:06 -1000

    Implements role list page

---
+++ src/pages/cards/RoleList.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {useRoleCardStore} from '/src/stores/roleCardStore.js';
+import {useRouter} from 'vue-router';
+import BackButton from 'src/components/button/BackButton.vue';
+
+const router = useRouter();
+const roleCardStore = useRoleCardStore();
+const roleList = roleCardStore.getRoles;
+
+const goToType = (role) => {
+  router.push('/cards/roles/' + role);
+};
+</script>
+
+<template>
+  <BackButton/>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <q-card
+        v-for="role in roleList"
+        :key="role"
+        @click="goToType(role)"
+        class="cursor-pointer hoverable"
+        flat
+        bordered
+      >
+        <q-card-section class="text-center">
+          <div class="text-h6 text-capitalize">{{ role }}</div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
