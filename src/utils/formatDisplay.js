fa8275100e86914945273a42fb37e90a7df0f12f
Brian Huffman <brianwkhuffman@gmail.com>
2025-08-06 21:48:20 -1000

    Adds new formatDisplay util file

---
+++ src/utils/formatDisplay.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {string} camelCaseString - A string to format.
+ * @returns {string} The string formatted with spaces and capitalization.
+ */
+function formatCamelCase(camelCaseString) {
+  return camelCaseString
+  .replace(/([A-Z])/g, ' $1')
+  .replace(/^./, str => str.toUpperCase());
+};
+
+
+export { formatCamelCase };
