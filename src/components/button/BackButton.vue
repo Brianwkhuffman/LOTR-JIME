4a0b5c4b28a4890320769dd6fc73863c2b34d1fd
Brian Huffman <brianwkhuffman@gmail.com>
2025-08-06 19:28:26 -1000

    Creates back and filter button

---
+++ src/components/button/BackButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {useRouter} from 'vue-router';
+
+const router = useRouter();
+
+const goBack = () => {
+  router.back();
+}
+</script>
+
+<template>
+  <q-btn @click="goBack">
+    Back
+  </q-btn>
+</template>
---
+++ src/components/button/FilterButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+const addFilter = () => {
+  return alert('filter');
+}
+</script>
+
+<template>
+  <q-btn @click="addFilter">
+    Filter
+  </q-btn>
+</template>
