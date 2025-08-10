02b7f7d4f884202ea281708e8124f4b188e99435
Brian Huffman <brianwkhuffman@gmail.com>
2025-08-09 23:43:00 -1000

    Creates DamageFear store, updates json desc

---
+++ src/stores/damageFearCardStore.js
@@ -0,0 +1,36 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import damageFearCardData from '/src/data/damageFearCards.json';
+
+/**
+ * @typedef {Object} DamageFearCard
+ * @property {number} id
+ * @property {string} name
+ * @property {string} description
+ * @property {number} amount
+ */
+
+/**
+ * @typedef {Object} DamageFearCardData
+ * @property {DamageFearCard[]} damage
+ * @property {DamageFearCard[]} fear
+ */
+
+export const useDamageFearCardStore = defineStore('damageFearCard', () => {
+  /** @type {import('vue').Ref<DamageFearCardData>} */
+  const cards = ref(damageFearCardData);
+
+  const getDamageCards = () => {
+    return cards.value.damage;
+  };
+
+  const getFearCards = () => {
+    return cards.value.fear;
+  };
+
+  return {
+    cards,
+    getDamageCards,
+    getFearCards
+  };
+});
