f2a269e849ddf95de95fa0ff4236122104550cfa
Brian Huffman <brianwkhuffman@gmail.com>
2025-08-28 21:08:05 -1000

    Style updates to hero step

--- src/components/deck/StepHero.vue
+++ src/components/deck/StepHero.vue
@@ -46,33 +46,38 @@ const validateHeroSelection = () => {
 </script>
 
 <template>
-  <div>
-    <div v-if="detailsLoading && deckLoading" class="row justify-center">
-      <q-spinner-oval color="primary" size="10rem" />
-    </div>
+  <div v-if="detailsLoading && deckLoading" class="row justify-center">
+    <q-spinner-oval color="primary" size="10rem" />
+  </div>
 
+  <div v-else style="display: grid; place-items: center;">
     <q-select
       :options="getHeroOptions"
       v-model="selectedHero"
+      label="Hero:"
       clearable
+      outlined
+      stack-label
+      color="primary"
+      style="width: 18rem;"
     />
-    <q-btn @click="selectHero">
-      Select Hero
-    </q-btn>
 
-    <div v-if="heroDetails">
-      <p>Suggested Role: {{heroDetails.suggestedRole}}</p>
-      <p>Suggested Equip: {{heroDetails.suggestedEquip}}</p>
-      <div v-if="heroDetails.cards">
-        <div
-          v-for="card in heroDetails.cards"
-          :key="card.name"
-          class="hero-card-preview q-pa-sm"
-          style="border: 1px solid black;"
-        >
-          <i>{{ card.name }}</i>
-          <p>{{ card.description }}</p>
-        </div>
+    <div class="q-pa-md">
+      <q-btn color="primary" @click="selectHero">
+        Next
+      </q-btn>
+    </div>
+
+    <div v-if="heroDetails?.cards.length">
+      <h6>{{ heroDetails.name }} cards:</h6>
+      <div
+        v-for="card in heroDetails.cards"
+        :key="card.number"
+        class="hero-card-preview q-pa-sm"
+        style="border: 1px solid black;"
+      >
+        <i>{{ card.name }}</i>
+        <p>{{ card.description }}</p>
       </div>
     </div>
   </div>
