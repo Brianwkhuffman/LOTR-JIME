6148da23c5e0d5d3ff117ea65d52acde33df36d4
Brian Huffman <brianwkhuffman@gmail.com>
2025-11-13 20:38:50 -1000

    Initial commit for equip tab WIP

--- src/components/deck/StepEquipment.vue
+++ src/components/deck/StepEquipment.vue
@@ -1,16 +1,41 @@
 <script setup>
+import { useEquipmentStore } from 'stores/equipmentStore.js';
+import { useDeckStore } from 'stores/deckStore';
+import { storeToRefs } from 'pinia';
+
+const equipStore = useEquipmentStore();
+const deckStore = useDeckStore();
 
 const emit = defineEmits(['nextStep']);
-const selectEquips = () => {
+const { loading: equipLoading } = storeToRefs(equipStore);
+const { loading: deckLoading } = storeToRefs(deckStore);
+
+const addEquipCards = () => {
   emit('nextStep');
 };
+
+// Armor/weapons/support all have 4 tiers
+// Trinkets all 3 tiers except Crown of Shadows (1 tier)
+// Mounts no tiers
 </script>
 
 <template>
-  <div>
-    <h3>Step Equip</h3>
-    <q-btn @click="selectEquips">
-      Select Equip
-    </q-btn>
+  <div v-if="equipLoading && deckLoading" class="row justify-center">
+    <q-spinner-oval color="primary" size="10rem" />
+  </div>
+
+  <div v-else style="display: grid; place-items: center;">
+
+    <div><p>Armor</p></div>
+    <div><p>Weapons</p></div>
+    <div><p>Trinket</p></div>
+    <div><p>Mount</p></div>
+
+    <div class="q-pa-md row justify-between">
+      <q-btn color="primary" @click="addEquipCards">
+        Select Equipment
+      </q-btn>
+    </div>
+
   </div>
 </template>
