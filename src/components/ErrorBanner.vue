a20d76fe3f055d4ac7d8aed7703f9c34cb2883df
Brian Huffman <brianwkhuffman@gmail.com>
2026-01-17 23:40:07 -1000

    Creates ErrorBanner component

---
+++ src/components/ErrorBanner.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { useDeckStore } from 'stores/deckStore';
+
+const deckStore = useDeckStore();
+
+defineProps({
+  error: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <transition
+    appear
+    enter-active-class="animated shake-box"
+  >
+    <q-banner class="bg-dark text-white q-mb-md rounded-borders">
+      <template v-slot:avatar>
+        <q-img src="assets/fear.png" style="width: 40px;" />
+      </template>
+      {{ error }}
+      <template v-slot:action>
+        <q-btn flat color="white" label="Dismiss" @click="deckStore.clearError" />
+      </template>
+    </q-banner>
+  </transition>
+</template>
+
+<style scoped>
+.q-img {
+  filter: invert(88%);
+}
+</style>
